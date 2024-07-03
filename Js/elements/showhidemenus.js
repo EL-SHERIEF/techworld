c933d9eecbf574a14a7bfc3fea029da535398502
EL-SHERIEF <ahmed-elsherief@outlook.com>
2024-07-03 20:21:36 +0300

    all of these

--- Js/elements/showhidemenus.js
+++ Js/elements/showhidemenus.js
@@ -1,28 +1,15 @@
 var menu = document.querySelector(".nav .menu");
 var subMenu = document.querySelector(".nav .menu .submenu");
+var susubbMenu = document.querySelector(".subsubmenu");
+
 var hiddenbutton = document.querySelector(".hidden-button")
 function hidemenu(){ hiddenbutton.style.opacity = "0"; menu.style.insetInlineEnd = "-100vw";hiddenbutton.style.display = "none"; };
 function showmenu(){hiddenbutton.style.opacity = "1";  menu.style.insetInlineEnd = "0vw";  hiddenbutton.style.display = "flex";}
-function showSubmenu(){subMenu.style.insetInlineEnd = "0vw";} function hideSubmenu(){subMenu.style.insetInlineEnd = "-100vw";}
-            
-var alert2 = document.querySelector(".alert.two");
-var alert1 = document.querySelector(".alert.one");
-function closeAlert() {
-    alert2.style.display = "none";
-}
+function showSubmenu(){subMenu.style.insetInlineEnd = "0vw";} 
+function hideSubmenu(){subMenu.style.insetInlineEnd = "-100vw";}
 
-var threed = document.querySelector(".spline");
-var vd = document.querySelector(".vd2");
-function show3d() {
-    threed.style.display = "block";
-    vd.style.display = "none";
-}
-function hide3d() {
-    threed.style.display = "none";
-    vd.style.display = "block";
-    alert1.style.display = "none";
-}
-
+function showsusubbMenu(){susubbMenu.style.display = 'flex';} 
+function hidesusubbMenu(){susubbMenu.style.display  = "none";}
 
 var projectMenu = document.querySelector(".discuss");
 var menublackscreen = document.querySelector(".discuss .blackscreen")
@@ -30,3 +17,8 @@ function hideprojectmenu(){projectMenu.style.opacity = "0"; menublackscreen.styl
 function showprojectmenu(){projectMenu.style.opacity = "1";menublackscreen.style.opacity = "0.7";  projectMenu.style.insetInlineEnd = "0vw";}
             
 
+
+var cart = document.querySelector(".cartt");
+function hideCart(){cart.style.opacity = "0";cart.style.insetInlineEnd = "-100vw";};
+function showCart(){cart.style.opacity = "1"; cart.style.insetInlineEnd = "0vw";}
+            
